ea4351d6d2900b917ab2e23107464917185f41d6
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-17 23:21:05 +0530

    number datatype

--- TypeScriptDemo/datatypes.ts
+++ TypeScriptDemo/datatypes.ts
@@ -3,3 +3,10 @@ let lname: string = "siraswa";
 
 let fullname: string = fname.toUpperCase().concat(lname.toUpperCase());
 console.log(fullname);
+
+let age: number;
+age = 25;
+
+let dob: string = "25";
+let date_of_birth: number = parseInt(dob);
+
