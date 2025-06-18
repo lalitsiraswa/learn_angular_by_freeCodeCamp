43ae13862a0a51cc54b2fcdaaee2f4c2817d2140
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-18 22:24:55 +0530

    array datatype

--- TypeScriptDemo/datatypes.ts
+++ TypeScriptDemo/datatypes.ts
@@ -12,3 +12,14 @@ let date_of_birth: number = parseInt(dob);
 
 let isValid: boolean = true;
 console.log("isValid: ", isValid);
+
+let empList: string[];
+empList = ["ramu", "rakesh", "anuj", "dharmesh"];
+let numList: Array<number>;
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numList.filter((num) => num % 2 == 0).forEach((num) => console.log(num));
+console.log(numList.find((num) => num === 11));
+console.log(
+  "Sum Of numList : ",
+  numList.reduce((acc, num) => acc + num, 5)
+); // 5 represents the initial value, default is 0;
