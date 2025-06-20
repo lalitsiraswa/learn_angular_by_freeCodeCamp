c6723cbcd3ed590a5a714042555fb5deebf2e0b7
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-20 22:34:31 +0530

    function with rest parameters

--- TypeScriptDemo/functions.js
+++ TypeScriptDemo/functions.js
@@ -1,10 +1,11 @@
 "use strict";
-// function with optional parameter
+// function with optional parameter.
 function add(num1, num2, num3) {
     return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(10, 20));
 console.log(add(10, 20, 30));
+// function with required parameter.
 const sub = (num1, num2, num3 = 2) => num1 - num2 - num3;
 console.log(sub(4, 5));
 console.log(sub(2, 4, 6));
@@ -12,3 +13,10 @@ const multi = function (num1, num2) {
     return num1 * num2;
 };
 console.log(multi(4, 5));
+// function with rest parameters.
+function add2(num1, num2, ...num3) {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+let numbers = [3, 4, 5, 6];
+console.log(add2(1, 2, ...numbers));
+console.log(add2(2, 4, ...[1, 2, 3, 4]));
--- TypeScriptDemo/functions.ts
+++ TypeScriptDemo/functions.ts
@@ -16,3 +16,11 @@ const multi = function (num1: number, num2: number): number {
   return num1 * num2;
 };
 console.log(multi(4, 5));
+
+// function with rest parameters.
+function add2(num1: number, num2: number, ...num3: number[]): number {
+  return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+let numbers: Array<number> = [3, 4, 5, 6];
+console.log(add2(1, 2, ...numbers));
+console.log(add2(2, 4, ...[1, 2, 3, 4]));
