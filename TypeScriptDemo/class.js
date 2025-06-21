26f267573c0946d14bd54ef2b507bdd25408e739
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-21 11:55:27 +0530

    Member Visibility or Access Modifiers

--- TypeScriptDemo/class.js
+++ TypeScriptDemo/class.js
@@ -1,17 +1,36 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Employee_id;
 // either undefined or ! for the erro: Property 'name' has no initializer and is not definitely assigned in the constructor.
 // In typescript you can only have either default constructor or parameterized constructor, but not both.
 // In typescript Multiple constructor implementations are not allowed.
 // When using constructor we don't need ! or undefined.
+// By-default all the member variables are public.
+// protected members are only accessible either inside the class or child class, these are not accessible through the object.
 class Employee {
     //   constructor() {}
     constructor(id, name, address) {
-        this.id = id;
+        _Employee_id.set(this, void 0); // private member variable.
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
         this.name = name;
         this.address = address;
     }
     getNameWithAddress() {
-        return `Name : ${this.name}\nAddress : ${this.address}`;
+        return `${this.name} stays at ${this.address}.`;
+    }
+}
+_Employee_id = new WeakMap();
+class Manager extends Employee {
+    constructor(id, name, address) {
+        super(id, name, address);
+    }
+    getNameWithAddress() {
+        return `${this.name} is a manager and stays at ${this.address}.`;
     }
 }
 // We can't have both the constructors.
@@ -24,3 +43,6 @@ class Employee {
 let john = new Employee(1, "John", "Highway 71");
 console.log(john);
 console.log(john.getNameWithAddress());
+let mike = new Manager(2, "Mike", "Church Street");
+console.log(mike);
+console.log(mike.getNameWithAddress());
--- TypeScriptDemo/class.ts
+++ TypeScriptDemo/class.ts
@@ -2,18 +2,29 @@
 // In typescript you can only have either default constructor or parameterized constructor, but not both.
 // In typescript Multiple constructor implementations are not allowed.
 // When using constructor we don't need ! or undefined.
+// By-default all the member variables are public.
+// protected members are only accessible either inside the class or child class, these are not accessible through the object.
 class Employee {
-  id: number;
-  name: string;
+  #id: number; // private member variable.
+  protected name: string;
   address: string;
   //   constructor() {}
   constructor(id: number, name: string, address: string) {
-    this.id = id;
+    this.#id = id;
     this.name = name;
     this.address = address;
   }
   getNameWithAddress(): string {
-    return `Name : ${this.name}\nAddress : ${this.address}`;
+    return `${this.name} stays at ${this.address}.`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(id: number, name: string, address: string) {
+    super(id, name, address);
+  }
+  getNameWithAddress(): string {
+    return `${this.name} is a manager and stays at ${this.address}.`;
   }
 }
 
@@ -28,3 +39,7 @@ class Employee {
 let john = new Employee(1, "John", "Highway 71");
 console.log(john);
 console.log(john.getNameWithAddress());
+
+let mike = new Manager(2, "Mike", "Church Street");
+console.log(mike);
+console.log(mike.getNameWithAddress());
