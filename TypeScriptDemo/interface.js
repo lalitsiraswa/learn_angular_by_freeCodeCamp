ca30ef86905af7f2583e87c5c3baa2efaf3dc434
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-21 15:10:23 +0530

    extends interface

--- TypeScriptDemo/interface.js
+++ TypeScriptDemo/interface.js
@@ -1,1 +1,10 @@
 "use strict";
+let user = { id: 1, name: "Robert", email: "robert@gmail.com" };
+console.log(user);
+let employee = {
+    id: 101,
+    name: "Mark",
+    email: "mark@gmail.com",
+    salary: 100000,
+};
+console.log(employee);
--- TypeScriptDemo/interface.ts
+++ TypeScriptDemo/interface.ts
@@ -1,6 +1,21 @@
 interface User {
   id: number;
   name: string;
-  age: number;
+  age?: number; // '?' makes this has an optional property.
   email: string;
 }
+
+interface Employees extends User {
+  salary: number;
+}
+
+let user: User = { id: 1, name: "Robert", email: "robert@gmail.com" };
+console.log(user);
+
+let employee: Employees = {
+  id: 101,
+  name: "Mark",
+  email: "mark@gmail.com",
+  salary: 100000,
+};
+console.log(employee);
