f2003b7007d79e882d11c6706733cdc7f3983e37
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-22 15:35:26 +0530

    Arrays Destructuring

--- TypeScriptDemo/interface.js
+++ TypeScriptDemo/interface.js
@@ -21,6 +21,7 @@ let users = [
     { id: 102, name: "DEF", email: "def@gmail.com" },
     { id: 103, name: "GHI", email: "ghi@gmail.com" },
 ];
+// Arrays Destructuring
 let [, user2, user3] = users;
 console.log(user2);
 console.log(user3);
