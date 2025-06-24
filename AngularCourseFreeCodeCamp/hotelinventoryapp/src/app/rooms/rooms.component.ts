d7d13f14c001497d2d82d34455b9cbc326ed5a56
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2025-06-24 10:01:18 +0000

    Interpolation Binding Syntax

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent implements OnInit {
-
+  hotelName = "Hilton Hotel";
   constructor() { }
 
   ngOnInit(): void {
