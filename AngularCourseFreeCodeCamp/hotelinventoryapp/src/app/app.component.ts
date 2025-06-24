b1b0aef2091b8bbccee47c26178e897db9b76881
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2025-06-24 09:32:01 +0000

    changes prefix name from app to hinv

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/app.component.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
   // template: "<h1>Hellp Geeks from inline template!</h1>", // If you want to add multiple lines then use `` instead of ''.
-  // styleUrls: ['./app.component.scss']
-  styles: [`h1{color: red}`]
+  styleUrls: ['./app.component.scss']
+  // styles: [`h1{color: red}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
