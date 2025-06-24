cd1d98b6dfa59a7a9736b3c0f9ddb169161a41c6
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2025-06-24 09:02:03 +0000

    HTML from inline template

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/app.component.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: "<h1>Hellp Geeks from inline template!</h1>",
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
