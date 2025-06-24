3451ecb7add86353b47365d56f88c65eb898faf5
Lalit Siraswa <50705460+lalitsiraswa@users.noreply.github.com>
2025-06-24 10:12:06 +0000

    Property Binding Syntax

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
   hotelName = "Hilton Hotel";
+  numberOfRooms = 20;
   constructor() { }
 
   ngOnInit(): void {
