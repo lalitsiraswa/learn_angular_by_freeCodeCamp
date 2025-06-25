f8c8b4d7ec0b75af2468956e1a4f404cfd7af004
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-25 22:09:34 +0530

    Changes Data to Date in RoomList Interface for checkinTime

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms.ts
@@ -1,17 +1,15 @@
-import { Data } from "@angular/router";
-
 export interface Room {
-    totalRooms: number;
-    availableRooms: number;
-    bookedRooms: number;
+  totalRooms: number;
+  availableRooms: number;
+  bookedRooms: number;
 }
 
 export interface RoomList {
-    roomNumber: number;
-    roomType: string;
-    amenities: string;
-    price: number;
-    photos: string;
-    checkinTime: Data;
-    checkoutTime: Date;
+  roomNumber: number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+  checkinTime: Date;
+  checkoutTime: Date;
 }
