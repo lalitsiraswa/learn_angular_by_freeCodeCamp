5b51655eb621caad0f1708c9ea1c9829943f67e6
Lalit Siraswa <lalitsiraswa@Lalits-MacBook-Pro.local>
2025-06-28 12:05:11 +0530

    ngOnChanges LifeCycle Hook

--- AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
+++ AngularCourseFreeCodeCamp/hotelinventoryapp/src/app/rooms/rooms-list/rooms-list.component.ts
@@ -3,8 +3,10 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnChanges,
   OnInit,
   Output,
+  SimpleChanges,
 } from '@angular/core';
 import { RoomList } from '../rooms';
 
@@ -14,13 +16,16 @@ import { RoomList } from '../rooms';
   styleUrls: ['./rooms-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
   // What the below line do @Input(): It will Make 'rooms' as a valid HTML property on 'hinv-rooms-list' HTML element,
   // so we can use property binding and send data from parent to child.
   @Input() rooms: RoomList[] = [];
   @Output() selectedRoom = new EventEmitter<RoomList>();
 
   constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('SimpleChanges : ', changes);
+  }
 
   ngOnInit(): void {}
 
